7e0c1ac2800868b9a8f57f307e2c8e7a9d6f5363
Sean Sutton <sean@thinknimble.com>
2019-05-26 00:17:06 -0400

    Event modifiers and key events

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line no-new
 new Vue({
   el: '#app',
   data: {
-    title: 'Becoming a Vue Ninja',
-    name: 'Sean',
-    url: 'http://www.youtube.com',
-    classes: ['one', 'two']
+    title: 'Vues from the 6',
+    coords: {
+      x: 0,
+      y: 0
+    }
   },
   methods: {
     greet() {
       return `Hello there`
+    },
+    logEvent(e) {
+      console.log(e)
+    },
+    logCoords(e) {
+      this.coords.x = e.offsetX
+      this.coords.y = e.offsetY
     }
   }
 })
